5f2994d6a213f340e62491716b022a28ce9716b9
susang233 <1350845315@qq.com>
2024-09-08 13:00:26 +0800

    清空购物车业务

--- sky-server/src/main/java/com/sky/controller/user/ShoppingCartController.java
+++ sky-server/src/main/java/com/sky/controller/user/ShoppingCartController.java
@@ -5,6 +5,7 @@ import com.sky.entity.ShoppingCart;
 import com.sky.result.Result;
 import com.sky.service.ShoppingCartService;
 import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
@@ -25,6 +26,7 @@ public class ShoppingCartController {
      * @return
      */
     @PostMapping("/add")
+    @ApiOperation("添加购物车")
     public Result add(@RequestBody ShoppingCartDTO shoppingCartDTO){
         log.info("添加购物车，商品信息为：{}",shoppingCartDTO);
         shoppingCartService.addShoppingCart(shoppingCartDTO);
@@ -33,8 +35,22 @@ public class ShoppingCartController {
     }
 
     @GetMapping("/list")
+    @ApiOperation("查看购物车")
     public Result<List<ShoppingCart>> list() {
+
        List<ShoppingCart> list=shoppingCartService.showShoppingCart();
        return Result.success(list);
+    }
+
+    /**
+     * 清空购物车
+     * @return
+     */
+    @DeleteMapping("/clean")
+    @ApiOperation("清空购物车")
+    public Result clean(){
+
+        shoppingCartService.cleanShoppingCart();
+        return Result.success();
     }
 }
